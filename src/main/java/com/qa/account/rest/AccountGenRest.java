79ea8d33df4e834bfec9066120505397eee4acbc
alvinjo <alvin.joseph0@gmail.com>
2018-11-28 10:58:43 +0000

    cors

--- src/main/java/com/qa/account/rest/AccountGenRest.java
+++ src/main/java/com/qa/account/rest/AccountGenRest.java
@@ -2,6 +2,7 @@ package com.qa.account.rest;
 
 import com.qa.account.util.constants.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,8 @@ public class AccountGenRest {
 
 	@Autowired
 	private AccountService service;
-	
+
+	@CrossOrigin(origins = "*")
 	@GetMapping(Constants.URL_CREATE_ACCOUNT)
 	public String createAccountNumber() {
 		return service.createAccountNumber();
